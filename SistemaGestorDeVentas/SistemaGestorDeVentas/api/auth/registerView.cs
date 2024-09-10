26140c811b050057fc577f5bb2ba810121c7a96f
altamiranoaxel77 <altamirano.programador@gmail.com>
2024-09-10 11:03:54 -0300

    division de carpetas

---
+++ SistemaGestorDeVentas/SistemaGestorDeVentas/api/auth/registerView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaGestorDeVentas.api.auth
+{
+    public partial class registerView : Form
+    {
+        public registerView()
+        {
+            InitializeComponent();
+        }
+    }
+}
